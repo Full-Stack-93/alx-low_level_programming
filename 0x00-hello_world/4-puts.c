408f6045289b36a54dc50d3066570a22879792c8
Deborah Ofori-Dartey <oforidartey.deborah@gmail.com>
2023-03-04 13:16:37 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the string "Programming is like building a multilingual puzzle"
+ * and a newline
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
